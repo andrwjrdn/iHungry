94f6ce0bceafd8c8c055e12d1b6a77fd4d0c336b
andrwjrdn <andrewjordanreyes@gmail.com>
2019-12-13 14:27:21 -0800

    able to pull data from API

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,8 +6,17 @@ const App = () => {
   
   const APP_ID = "9fb7d1e8";
   const APP_KEY = "df9da258605c678894a8dd1e936d1271";
+  
+  useEffect(() => {
+    getRecipes();
+  }, []);
 
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const getRecipes = async () => {
+    const response = await fetch (`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    console.log(data.hits);
+  }
 
   return (
     <div className="App">
